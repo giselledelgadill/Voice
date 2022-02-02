4381d94d43ad3d2e25292ccb7286a9339ecaeba9
giselledelgadill <97265938+giselledelgadill@users.noreply.github.com>
2022-02-01 17:58:27 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -9,16 +9,35 @@ recognition.onresult=function run(event){
     var content=event.results[0][0].transcript;
     document.getElementById("textbox").innerHTML=content;
     console.log(content);
+if(content=="take my selfie"){
+    console.log("taking your selfie"); 
     speak();
+}
+  
 }
 function speak(){
     var synth=window.speechSynthesis;
-    speak_data=document.getElementById("textbox").value;
+    speak_data="taking your selfie in 10 seconds";
     var utterThis=new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
 Webcam.attach(camera);
+setTimeout (function(){
+    take_snapshot();
+    save();
+},10000);
 }
 Webcam.set({
     width:400,height:300,image_format:'png',png_quality:90
 });
 camera=document.getElementById("camera");
+function take_snapshot(){
+    Webcam.snap(function(data_uri){
+        document.getElementById("result").innerHTML='<img id="math"src="'+data_uri+'">';
+    });
+}
+function save(){
+link=document.getElementById("link");
+image=document.getElementById("math").src;
+link.href=image;
+link.click();
+}
